0f6b7e32b24d4365122c78aed10c7133d3b42393
Alex Tzeng <alxtz.tw@gmail.com>
2019-07-26 16:15:54 +0800

    feature: for gh page hosting

---
+++ build/precache-manifest.946945d7fee1f49414e398d1d9290b9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56f947becd7362fb83e0a9f062c19b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be8133024a9975c65c0a",
+    "url": "/static/css/main.7462157a.chunk.css"
+  },
+  {
+    "revision": "1a9195550e1758c16073",
+    "url": "/static/js/2.b7965660.chunk.js"
+  },
+  {
+    "revision": "be8133024a9975c65c0a",
+    "url": "/static/js/main.fde7020d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
